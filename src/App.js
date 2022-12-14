2890020d81ceac40dc6747fd76a8403351e05567
ramontcruz <115509915+ramontcruz@users.noreply.github.com>
2022-12-14 15:45:44 -0300

    inicio da edição de profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Signup } from "./pages/Signup";
+import { Home } from "./pages/Home/index.js";
+import { Login } from "./pages/Login/index.js";
+import { Signup } from "./pages/Signup/index.js";
 import { AuthContextComponent } from "./contexts/authContext";
-import { Profile } from "./pages/Profile";
-import { ErrorPage } from "./pages/ErrorPage";
+import { Profile } from "./pages/Profile/index.js";
+import { ErrorPage } from "./pages/ErrorPage/index.js";
 
 import { ProtectedRoute } from "./components/ProtectedRoute";
 
